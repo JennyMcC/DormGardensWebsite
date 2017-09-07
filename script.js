4776f979b8ee7dc69bc07451a8062135fa81b847
JennyMcC <jennymccargish@gmail.com>
2017-09-06 21:09:06 -0400

    started js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var Plant = function(name, picture, price, planterColor) {
+	this.name = name;
+	this.picture = picture;
+	this.price = price;
+	this.planterColor = planterColor;
+};
+
+var plant1 = new Plant('Ironwood Succulent Planter', "images/plant1.jpg", '$15', 'medium');
+var plant2 = new Plant('Marblewood Succulent Planter', 'images/plant2.jpg', '$15', 'light');
+var plant3 = new Plant('African Padauk Succulent Planter', 'images/plant3.jpg', '$15', 'medium');
+
+
+
+
+
+
