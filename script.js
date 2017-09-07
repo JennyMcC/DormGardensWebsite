8adf809c0764abd8f3544c55bd5f694ee2fcd722
JennyMcC <jennymccargish@gmail.com>
2017-09-07 12:34:46 -0400

    styling

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ var plant3 = new Plant('African Padauk Succulent Planter', '3', '$15', 'medium')
 // when a filter is clicked on:
 document.querySelector('.filter').addEventListener('click', function() {
 
+	for
+
+
 	var galleryDOM = document.querySelector('.plant');
 		galleryDOM.src = 'images/plant' + this.picture + '.jpg';
 
