c1721056d132c9cd862025dd253c132f5c3b047d
JennyMcC <jennymccargish@gmail.com>
2017-09-07 11:54:20 -0400

    trying

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ var Plant = function(name, picture, price, planterColor) {
 	this.planterColor = planterColor;
 };
 
-var plant1 = new Plant('Ironwood Succulent Planter', "images/plant1.jpg", '$15', 'medium');
-var plant2 = new Plant('Marblewood Succulent Planter', 'images/plant2.jpg', '$15', 'light');
-var plant3 = new Plant('African Padauk Succulent Planter', 'images/plant3.jpg', '$15', 'medium');
+var plant1 = new Plant('Ironwood Succulent Planter', '1', '$15', 'medium');
+var plant2 = new Plant('Marblewood Succulent Planter', '2', '$15', 'light');
+var plant3 = new Plant('African Padauk Succulent Planter', '3', '$15', 'medium');
+
+
+
+
+
+
+
+// when a filter is clicked on:
+document.querySelector('.filter').addEventListener('click', function() {
+
+	var galleryDOM = document.querySelector('.plant');
+		galleryDOM.src = 'images/plant' + this.picture + '.jpg';
+
+});
 
 
 
