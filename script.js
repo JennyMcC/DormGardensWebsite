42230998665236b1726342d02d437ac7bb01751f
JennyMcC <jennymccargish@gmail.com>
2017-09-07 19:17:29 -0400

    possible setup

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 
-var Plant = function(name, picture, price, planterColor) {
-	this.name = name;
-	this.picture = picture;
-	this.price = price;
-	this.planterColor = planterColor;
-};
+var gardenController = (function() {
+
+	var Plant = function(name, picture, price, planterColor) {
+		this.name = name;
+		this.picture = picture;
+		this.price = price;
+		this.planterColor = planterColor;
+	};
 
 var plant1 = new Plant('Ironwood Succulent Planter', '1', '$15', 'medium');
 var plant2 = new Plant('Marblewood Succulent Planter', '2', '$15', 'light');
 var plant3 = new Plant('African Padauk Succulent Planter', '3', '$15', 'medium');
 
 
+})();
 
 
+
+
+
+var UIController = (function() {
+
+	var DOMStrings = {
+
+	};
+
+
+})();
+
+
+
+
+var controller = (function(gardenCtrl, UICtrl) {
+
+})(gardenController, UIController);
+
+controller.intit();
 
 
 
